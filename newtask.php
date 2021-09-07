7118842169e17b08c358b444cee569126b642ec5
lyesbk <boukbouka.lyes@outlook.fr>
2021-09-07 14:13:12 +0200

    add newtask.php

--- newtask.php
+++ newtask.php
@@ -3,7 +3,8 @@ session_start();
 
 include("./connexion_bdd.php");
 
-
+//variable de session board_id
+$board_id = 213;
 $email = $_SESSION["email"];
 
 echo($email);
@@ -25,4 +26,35 @@ $result = $stmt->fetch();
 }
 
 $id = $result['id'];
+$taskresp = $_POST['responsible'];
+$tasktitle = $_POST['titre'];
+$taskdesc = $_POST['description'];
+$taskstatus = $_POST['status'];
+$taskcategory = $_POST['category'];
+$taskend = $_POST['enddate'];
+$taskdiff = $_POST['difficulty'];
+$taskpoint = $_POST['points'];
 
+try {
+    // Création de la requête
+    $rqt = "INSERT INTO tasks (board_id, author_id, titre, 'description', end_date, responsible, 'status', category, 'point', difficulty) VALUES (:board_id, :author_id, :titre, :dsc, :end_date, :responsible, :statu, :category, :pts, :difficulty)";
+    
+    $stmt = $conn->prepare($rqt);
+    
+    $stmt->bindParam(':boar_id', $board_id );
+    $stmt->bindParam(':author_id', $id);
+    $stmt->bindParam(':titre', $tasktitle);
+    $stmt->bindParam(':dsc', $taskdesc);
+    $stmt->bindParam(':end_date', $taskend);
+    $stmt->bindParam(':responsible', $taskresp);
+    $stmt->bindParam(':statu', $taskstatus);
+    $stmt->bindParam(':category', $taskcategory);
+    $stmt->bindParam(':pts', $taskpoint);
+    
+    $stmt->execute();
+    echo("Nouvelle tâche ajoutée !");
+    
+    
+    } catch(Exception $e) {
+        echo "Problème rencontré lors de l'ajout de la tâche : " . $e->getMessage();
+    }
